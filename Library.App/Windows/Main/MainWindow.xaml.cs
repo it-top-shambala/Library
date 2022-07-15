7da222084c5414cffbcdb24de6fab847fc450da9
Starinin Andrey <starininandrey@gmail.com>
2022-07-15 21:21:25 +0300

    Init

---
+++ Library.App/Windows/Main/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using Library.Model;
+
+namespace Library.App.Windows.Main;
+
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    public ObservableCollection<Book> Books { get; set; }
+
+    private Book _book;
+
+    public Book Book
+    {
+        get => _book;
+        set
+        {
+            if (value is null)
+            {
+                return;
+            }
+
+            if (value == _book)
+            {
+                return;
+            }
+
+            _book = value;
+            OnPropertyChanged(nameof(Book));
+        }
+    }
+
+    public MainWindow()
+    {
+        _book = new Book();
+        Books = new ObservableCollection<Book>();
+
+        InitializeComponent();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void Clear()
+    {
+        InputTitle.Clear();
+        InputAuthor.Clear();
+        InputGenre.Clear();
+    }
+
+    private void ButtonClear_OnClick(object sender, RoutedEventArgs e)
+    {
+        Clear();
+    }
+
+    private void ButtonDelete_OnClick(object sender, RoutedEventArgs e)
+    {
+        Books.Remove(Book);
+        Clear();
+    }
+
+    private void ButtonSave_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (!Books.Contains(Book))
+        {
+            Books.Add(Book);
+        }
+
+        Clear();
+    }
+}
---
+++ Library.Model/Book.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Library.Model;
+
+public class Book : INotifyPropertyChanged
+{
+    private string _title;
+
+    public string Title
+    {
+        get => _title;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return;
+            }
+
+            if (value == _title)
+            {
+                return;
+            }
+
+            _title = value;
+            OnPropertyChanged(nameof(Title));
+        }
+    }
+
+    private string _author;
+
+    public string Author
+    {
+        get => _author;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return;
+            }
+
+            if (value == _author)
+            {
+                return;
+            }
+
+            _author = value;
+            OnPropertyChanged(nameof(Author));
+        }
+    }
+
+    private string _genre;
+
+    public string Genre
+    {
+        get => _genre;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return;
+            }
+
+            if (value == _genre)
+            {
+                return;
+            }
+
+            _genre = value;
+            OnPropertyChanged(nameof(Genre));
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
